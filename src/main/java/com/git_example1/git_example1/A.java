31c198653665f838ac1877411db998d9010c2963
kalimullah <kalimullah41557@gmail.com>
2024-12-18 21:46:36 +0530

    defect fixed -3560 - Fixed null values not to be store in database

--- src/main/java/com/git_example1/git_example1/A.java
+++ src/main/java/com/git_example1/git_example1/A.java
@@ -1,4 +1,11 @@
 package com.git_example1.git_example1;
 
 public class A {
-}
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+        System.out.println("This is a new feature added by user2.");
+        System.out.println("This is a bug fix added by user3.");
+    }
+ }
---
+++ src/main/java/com/git_example1/git_example1/B.java
@@ -0,0 +1,7 @@
+package com.git_example1.git_example1;
+
+public class B {
+    public void test(){
+        System.out.println("B is running");
+    }
+}
