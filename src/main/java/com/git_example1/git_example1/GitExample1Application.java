c957621533d32c332a59d8e2feb0e43bf57ad09a
kalimullah <kalimullah41557@gmail.com>
2024-12-18 21:27:45 +0530

    Defect 3450 -  Fixed the Links not were not working home page

---
+++ src/main/java/com/git_example1/git_example1/GitExample1Application.java
@@ -0,0 +1,13 @@
+package com.git_example1.git_example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/git_example1/git_example1/GitExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_example1.git_example1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
